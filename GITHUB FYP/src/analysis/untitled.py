6970b354b17ac7d3fc3cdde0e30c94699068a52f
AHMED011677 <230316874@aston.ac.uk>
2026-04-15 18:49:36 +0100

    Delete GITHUB FYP directory

--- GITHUB FYP/src/analysis/untitled.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-data = np.load(r"C:\Users\ahmed\OneDrive\FINAL PROJECT\data\processed\labelled_dataset.npz")
-
-X = data["X"]
-y = data["y"]
-
-print("X shape:", X.shape)
-print("y:", y)
